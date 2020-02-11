b1c846ae3bb7ba140b431fa474a353afebebe56f
Genada Zoto <genadazoto@hotmail.com>
2020-02-11 16:42:55 +0100

    object stringify

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,4 +25,23 @@ class SessionController extends AbstractController
     return $this->render('session/afficher_variable.html.twig',
         ['nom'=>$nom]);
    }
+
+   /**
+     * @Route("/session/creer/variable/objet")
+     */
+    public function creerVariableObjet(SessionInterface $objSession){
+        $objDate= new DateTime();
+        $objSession->set('uneDate',$objDate);
+        return $this->render('session/creer_variable_objet.html.twig');
+       }
+
+    /**
+     * @Route("/session/afficher/variable/objet")
+     */
+    public function afficherVariableObjet(SessionInterface $objSession){
+       
+        $objDate = $objSession->get('uneDate');
+        return $this->render('session/afficher_variable_objet.html.twig',
+        ['uneDate'=>$objDate]);
+       }
 }
